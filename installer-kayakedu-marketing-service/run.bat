3d38a211a9fe92b3e657ee6d7b0e510b9243bd7d
Ramakrishnan Manoharan <ramakrishnan.manoharan@cioxhealth.com>
2022-11-02 20:52:51 +0530

    updated

--- installer-kayakedu-marketing-service/run.bat
+++ installer-kayakedu-marketing-service/run.bat
@@ -4,7 +4,7 @@ echo Welcome to kayakedu scripting.
 
 IF "%1"=="start" (
     ECHO start kayakedu-marketing-service
-    start "kayakedu-marketing-service" java -Dspring.config.location="D:/kayakedu-marketing-service/application.yaml" -jar kayakedu-marketing-services.jar
+    start "kayakedu-marketing-service" java -Dspring.config.location="application.yaml" -jar kayakedu-marketing-services.jar
 	exit
 ) ELSE IF "%1"=="stop" (
     ECHO stop kayakedu-marketing-service
